fbba14d72e0774fdd0f044f9ac9d23f02d2a964e
Jon <jonurnieta@gmail.com>
2025-12-04 22:34:55 +0100

    feat: Implement interactive Leaflet map

--- src/settings.ts
+++ src/settings.ts
@@ -1,3 +1,9 @@
 export const mail = 'jonurnieta@gmail.com';
 export const github = 'jramosg';
 export const linkedin = 'jon-ramos-8ba55a14a';
+export const location = {
+  city: 'Urnieta',
+  lat: 43.248,
+  lng: -1.994,
+};
+export const company = 'Jon Ramos';
